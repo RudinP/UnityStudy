b4e708381be394c2300098f7ce8a37a716231feb
RudinP <65944851+RudinP@users.noreply.github.com>
2021-03-08 21:19:11 +0900

    Update Rotator.cs

--- Code/Rotator.cs
+++ Code/Rotator.cs
@@ -8,6 +8,6 @@ public class Rotator : MonoBehaviour
     
     void Update()
     {
-        transform.Rotate(60*Time.deltaTime, 60*Time.deltaTime, 60*Time.deltaTime);
+        transform.Rotate(60*Time.deltaTime, 60*Time.deltaTime, 60*Time.deltaTime); //Update()는 매 초가 아닌 프레임. 따라서 Time.deltaTime을 통해 초마다로 변경해줌.
     }
 }
